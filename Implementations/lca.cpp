c8de60a41f438000f24ec43a1734fbfcc2aade9b
Jay7221 <jns29072003@gmail.com>
2023-07-23 14:37:24 +0530

    created separate lca structs for vector graph and class graph

--- Implementations/lca.cpp
+++ Implementations/lca.cpp
@@ -1,4 +1,60 @@
-class LCA{
+struct LCA{
+    vector<int> depth;
+    vector<vector<int>> par;
+    vector<vector<int>> adj;
+    const int LOGMAX = 20;
+    LCA(vector<vector<int>> &adj) : adj(adj){
+        int n = adj.size();
+
+        depth = vector<int>(n);
+        par = vector<vector<int>>(n, vector<int>(LOGMAX, -1));
+
+        dfs(0);
+
+        for(int k = 1; k < LOGMAX; ++k){
+            for(int node = 0; node < n; ++node){
+                if(par[node][k - 1] != -1){
+                    par[node][k] = par[par[node][k - 1]][k - 1];
+                }
+            }
+        }
+    }
+    void dfs(int node, int p = -1){
+        par[node][0] = p;
+        for(int v : adj[node]){
+            if(v != p){
+                depth[v] = depth[node] + 1;
+                dfs(v, node);
+            }
+        }
+    }
+    int lca(int u, int v){
+        if(depth[u] < depth[v]){
+            swap(u, v);
+        }
+        int diff = depth[u] - depth[v];
+        for(int k = 0; k < LOGMAX; ++k){
+            if((diff >> k) & 1){
+                u = par[u][k];
+            }
+        }
+        if(u == v){
+            return u;
+        }
+        for(int k = LOGMAX - 1; k >= 0; --k){
+            if(par[u][k] != par[v][k]){
+                u = par[u][k];
+                v = par[v][k];
+            }
+        }
+        u = par[u][0];
+        return u;
+    }
+    int dist(int u, int v){
+        return ((depth[u] + depth[v]) - 2 * depth[lca(u, v)]);
+    }
+};
+class LCA_g{
     public:
         const int LOGMAX = 30;
         Graph g;
